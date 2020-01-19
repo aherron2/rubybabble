219d2ac6f39875de511ff5a477ad781e72e02965
Amy Herron <aherron2@my.westga.edu>
2020-01-18 21:33:15 -0500

    fixed issues in testing

---
+++ tests/tile_bag/test_draw_tile.rb
@@ -0,0 +1,15 @@
+require './tile_bag.rb'
+require "minitest/autorun"
+
+class TileBag::TestDrawTile < Minitest::Test
+
+  def test_has_proper_number_of_tiles
+  #tests that exactly 98 tiles can be drawn (after which the bag is empty)
+  
+  end
+  
+  def test_has_proper_tile_distribution
+  #tests that the tiles in the bag are distributed exactly as specified in the Scrabble rules.
+  end
+  
+end
--- tests/tile_bag/test_points_for.rb
+++ tests/tile_bag/test_points_for.rb
@@ -10,7 +10,7 @@ class TileBag::TestPointsFor < Minitest::Test
       tiles_three_points = [:B, :C, :M, :P]
       tiles_four_points = [:F, :H, :V, :W, :Y]
       tiles_five_points = [:K]
-      tiles_eight_points = [:J, :K]
+      tiles_eight_points = [:J, :X]
       tiles_ten_points = [:Q, :Z]
 
       
@@ -18,28 +18,28 @@ class TileBag::TestPointsFor < Minitest::Test
         assert_equal 1, TileBag.points_for(tile)
       end
 
-      #tiles_two_points.each do |tile|
-      #  assert_equals(2, TileBag.points_for(tile))
-      #end
-      #
-      #tiles_three_points.each do |tile|
-      #  assert_equals(3, TileBag.points_for(tile))
-      #end
-      #
-      #tiles_four_points.each do |tile|
-      #  assert_equals(4, TileBag.points_for(tile))
-      #end
-      #
-      #tiles_five_points.each do |tile|
-      #  assert_equals(5, TileBag.points_for(tile))
-      #end
-      #
-      #tiles_eight_points.each do |tile|
-      #  assert_equals(8, TileBag.points_for(tile))
-      #end
-      #
-      #tiles_ten_points.each do |tile|
-      #  assert_equals(10, TileBag.points_for(tile))
-      #end
+      tiles_two_points.each do |tile|
+        assert_equal(2, TileBag.points_for(tile))
+      end
+      
+      tiles_three_points.each do |tile|
+        assert_equal(3, TileBag.points_for(tile))
+      end
+      
+      tiles_four_points.each do |tile|
+        assert_equal(4, TileBag.points_for(tile))
+      end
+      
+      tiles_five_points.each do |tile|
+        assert_equal(5, TileBag.points_for(tile))
+      end
+      
+      tiles_eight_points.each do |tile|
+        assert_equal(8, TileBag.points_for(tile))
+      end
+      
+      tiles_ten_points.each do |tile|
+        assert_equal(10, TileBag.points_for(tile))
+      end
     end
 end
